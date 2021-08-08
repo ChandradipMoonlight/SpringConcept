7e04140572f7717bca2100fbb4162ec7c3d87fb5
ChandradipMoonlight <chandradipshivankar@gmail.com>
2021-08-08 12:13:49 +0530

    created project to learn spring concept

---
+++ src/main/java/com/example/springconcept/SpringConceptApplication.java
@@ -0,0 +1,14 @@
+package com.example.springconcept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConceptApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Spring concept demo");
+		SpringApplication.run(SpringConceptApplication.class, args);
+	}
+
+}
